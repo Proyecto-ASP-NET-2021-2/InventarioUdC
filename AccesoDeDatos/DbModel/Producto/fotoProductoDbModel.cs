a7885ed282d891e30893177929c951f9d9f5f063
Juan David Osorio <juandavidoh3@gmail.com>
2021-12-13 19:19:47 -0500

    Algunos namespaces corregidos

--- AccesoDeDatos/DbModel/Producto/fotoProductoDbModel.cs
+++ AccesoDeDatos/DbModel/Producto/fotoProductoDbModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AccesoDeDatos.DbModel.Parametros
+namespace AccesoDeDatos.DbModel.Producto
 {
     public class fotoProductoDbModel
     {
